6170bded67c219761f7370f0015e848d8793ec75
Sayak Sarkar <sayaksarkar73@gmail.com>
2022-01-20 17:48:01 +0530

    Landing Page Commit
    Landing Page Introduced with basic HTML,CSS and Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        brand:{
+          DEFAULT: "#0D0D2B",
+          button: "#4A1FCB",
+          sec_color: "#953E97"
+        }
+      }
+    },
+    fontFamily: {
+      open_sans: ['Open Sans'],
+      alpha_slab: ['Alfa Slab One']
+    }
+  },
+  plugins: [],
+}
